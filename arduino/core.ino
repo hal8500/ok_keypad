6d7c44bd728b7a929525e7dc625f526801a23164
Haruki Suzuki <h-suzuki@papas.jp>
2025-05-12 16:50:31 +0900

    keytable作成
    TODO: buttonAssingの削除

--- arduino/core.ino
+++ arduino/core.ino
@@ -1,83 +1,131 @@
 const int NUM_ITEMS = 9;
 const String FILE_PATH = "/j.json";
 
-
 /*
-items = [
-  "text",
-  {name: string, actions:[ {type:"print"|"press(ID)?"|"release(ID)?"|"write(ID)?"|"releaseAll"|"delay"|"move"|"click", arg:int|string, x:int, y|int} ]},
-]
-
 # pressとpressIDの違い 
 pressはcharとして出力する。pressIDはUSB HID Usage IDとして出力する？
 
+https://github.com/HisashiKato/KeyboardPicoJP
 https://bsakatu.net/doc/usb-hid-to-scancode/
+
+JSONVarの読み出しについて
+存在しないプロパティや型が一致しない読み出しを行うと型のデフォルト値が返される
+JSONVar json = JSON.parse("{ a: "aa" }")
+int x = (int)json["a"] // x = 0
+int y = (int)json["?"] // y = 0
+
+文字列のJSONVarにlength()を呼び出すと文字列の長さを返す
 */
 
 const String DEFAULT_ITEMS_JSON = R"(
+[
+  "1",
+  {"type":"hello world"},
+  {"button":"a"},
+  {"button":"TAB"},
+  [{"name":"sample macro 1"},{"type":"this is macro 1"}],
+  [{"name":"sample macro 2","description":"これは説明用のサンプルマクロです"},
+  {"press":"a"},{"delay":1000},{"release":"ALL"}],
+  [{"name":"sample macro 3","description":"これは説明用のサンプルマクロです。長いタイプの説明文です。あああああ本日は晴天なり"},{"press":"a"},{"delay":1000},{"release":"ALL"},{"click":"LEFT"},{"move":{"x":10,"y":30}}],
+  [{"press":"r"},{"delay":1000},{"release":"ALL"},{"type":"macro"}],
   [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    {
-        "name": "chrome setup",
-        "actions": [
-            {
-                "type": "print",
-                "arg": "\t"
-            },
-            {
-                "type": "pressID",
-                "arg": 81
-            },
-            {
-                "type": "delay",
-                "arg": 1500
-            },
-            {
-                "type": "releaseAll"
-            },
-            {
-                "type": "print",
-                "arg": " "
-            },
-            {
-                "type": "delay",
-                "arg": 500
-            },
-            {
-                "type": "print",
-                "arg": "aaaaaa"
-            },
-            {
-                "type": "print",
-                "arg": "\t"
-            },
-            {
-                "type": "print",
-                "arg": "p"
-            },
-            {
-                "type": "print",
-                "arg": "\t"
-            },
-            {
-                "type": "print",
-                "arg": "password"
-            }
-        ]
-    }
+    {"name":"chrome setup","description":"chromeのセットアップ自動化マクロ"},
+    {"tap":"TAB"},
+    {"press":"DOWN_ARROW"},
+    {"delay":1500},
+    {"release":"DOWN_ARROW"},
+    {"type":" "},
+    {"delay":500},
+    {"type":"ssid aaaa"},
+    {"tap":"TAB"},
+    {"tap":"p"},
+    {"tap":"TAB"},
+    {"type":"password"}
+  ]
 ]
+)";
+
+const String KEY_TABLE_JSON = R"(
+  {
+  "LEFT_CTRL": 224,
+  "LEFT_SHIFT": 225,
+  "LEFT_ALT": 226,
+  "LEFT_GUI": 227,
+  "RIGHT_CTRL": 228,
+  "RIGHT_SHIFT": 229,
+  "RIGHT_ALT": 230,
+  "RIGHT_GUI": 231,
+  "RETURN": 40,
+  "ESC": 41,
+  "BACKSPACE": 42,
+  "TAB": 43,
+  "CAPS_LOCK": 57,
+  "PRINT_SCREEN": 70,
+  "SCROLL_LOCK": 71,
+  "PAUSE": 72,
+  "INSERT": 73,
+  "HOME": 74,
+  "PAGE_UP": 75,
+  "DELETE": 76,
+  "END": 77,
+  "PAGE_DOWN": 78,
+  "RIGHT_ARROW": 79,
+  "LEFT_ARROW": 80,
+  "DOWN_ARROW": 81,
+  "UP_ARROW": 82,
+  "MENU": 101,
+  "NUM_LOCK": 83,
+  "KP_SLASH": 84,
+  "KP_ASTERISK": 85,
+  "KP_MINUS": 86,
+  "KP_PLUS": 87,
+  "KP_ENTER": 88,
+  "KP_1": 89,
+  "KP_2": 90,
+  "KP_3": 91,
+  "KP_4": 92,
+  "KP_5": 93,
+  "KP_6": 94,
+  "KP_7": 95,
+  "KP_8": 96,
+  "KP_9": 97,
+  "KP_0": 98,
+  "KP_DOT": 99,
+  "F1": 58,
+  "F2": 59,
+  "F3": 60,
+  "F4": 61,
+  "F5": 62,
+  "F6": 63,
+  "F7": 64,
+  "F8": 65,
+  "F9": 66,
+  "F10": 67,
+  "F11": 68,
+  "F12": 69,
+  "F13": 104,
+  "F14": 105,
+  "F15": 106,
+  "F16": 107,
+  "F17": 108,
+  "F18": 109,
+  "F19": 110,
+  "F20": 111,
+  "F21": 112,
+  "F22": 113,
+  "F23": 114,
+  "F24": 115,
+  "LEFT": 1,
+  "RIGHT": 2,
+  "MIDDLE": 4
+}
+    
 )";
 
 class Okeypad {
 private:
   bool isFileLoaded;
+  JSONVar keyTable;
 
   void set(String &command);
   void setJson(String &command);
@@ -96,12 +144,14 @@ public:
   void handle(String &command);
   void load();
   void exec(int slot);
+  void exec_command(const JSONVar &item);
 };
 
 void Okeypad::init() {
   isFileLoaded = false;
   debug = false;
 
+  keyTable = JSON.parse(KEY_TABLE_JSON);
   items = JSON.parse(DEFAULT_ITEMS_JSON);
 }
 
@@ -216,6 +266,9 @@ void Okeypad::exec(int code) {
   }
 
   JSONVar item = items[code];
+  exec_command(item);
+
+
   if (JSON.typeof(item) == "string") {
     Keyboard.print((const char *)item);
   } else {
@@ -290,6 +343,25 @@ void Okeypad::help() {
   Serial.println("- help               Print this message. ");
   Serial.println();
   Serial.println();
+}
+
+void Okeypad::exec_command(const JSONVar &item) {
+  if (JSON.typeof(item) == "string") {
+    Keyboard.print((const char *)item);
+  } else if (JSON.typeof(item) == "number") {
+    delay((unsigned long)item);
+  } else if (JSON.typeof(item) == "object") {
+    if (item.hasOwnProperty("type")) {
+      String arg = (String)item["type"];
+      Keyboard.print(arg);
+    } else if (item.hasOwnProperty("click")) {
+      String arg = (String)item["click"];
+      if (keyTable.hasOwnProperty(arg)) {
+        int btn = (int)keyTable[arg];
+        Mouse.click(btn);
+      }
+    }
+  }
 }
 
 Okeypad okeypad;
--- src/routes/pg/+page.svelte
+++ src/routes/pg/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { browser } from "$app/environment";
+    import { KEY_TABLE } from "$lib/types";
 
     const ITEM_KEY = "pg_test_value";
     let ts_value: string | null = $state(null);
@@ -17,3 +18,9 @@
 <h1>hello</h1>
 <p>{ts_value}</p>
 <input type="text" bind:value={ts_value} />
+<hr>
+<pre>
+    <code>
+        {JSON.stringify(KEY_TABLE, null, 2)}
+    </code>
+</pre>
